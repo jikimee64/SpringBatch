14f95a29037f5b849b5ee4989ba7cf2490c6307d
WooCheol Kim <jikimee64@gmail.com>
2020-11-09 23:23:57 +0900

    환경설정

---
+++ src/test/java/com/soap/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
